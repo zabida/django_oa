029ee9b8e96a748c31cbfeedf9e6dcaa57addfc0
zabida <8888@qq.com>
2020-12-11 13:55:35 +0800

    升级

--- utils/orm_test.py
+++ utils/orm_test.py
@@ -1,27 +1,16 @@
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'tutorial.settings')
+
 import django
+
+django.setup()
+
 from account import models as acc_mod
 from snippets import models as sni_mod
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'tutorial.settings')
-django.setup()
-
-user = acc_mod.User.objects.get(pk=1)
-print(user)
-
-sni = sni_mod.Snippet.objects.get(pk=1)
-print(sni.owner)
-print(sni.owner_id)
-
-
-# 反向查询
-user: sni_mod.Snippet = acc_mod.User.objects.get(pk=1)
-# print(user.snippet_set.all())  默认是小写的类名_set
-print(user.owner_snippets.all())   # 定义模型时Snippet里定义了owner_snippets关系名
-
-a = sni_mod.Snippet.objects.filter(title='haha').values('id', 'code', 'created')
-# a = [{'id': '', 'code': '', 'created': ''}, {}, {}]
-b = sni_mod.Snippet.objects.filter(title='haha').values_list('id', 'code', 'created')
-# b = [('id', 'code', 'created'), (), ()...]
-c = sni_mod.Snippet.objects.filter(title='haha').values_list('code', flat=True)
-# c = ['code', ...]
+objs = acc_mod.User.objects.filter()
+objs.filter(password=12345).update(password=1)   # 返回更新个数
+print(objs)
+for i in objs.all():
+    print(i.__dict__)
