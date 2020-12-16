5d3f2b9562ec71ea4d47539a1fb9e37cdd61b38e
zabida <8888@qq.com>
2020-12-16 13:59:32 +0800

    升级

--- utils/orm_demo.py
+++ utils/orm_demo.py
@@ -1,5 +1,7 @@
 import os
 import django
+from django.db.models import Q
+
 from account import models as acc_mod
 from snippets import models as sni_mod
 
@@ -21,7 +23,24 @@ print(user.owner_snippets.all())   # 定义模型时Snippet里定义了owner_sni
 
 a = sni_mod.Snippet.objects.filter(title='haha').values('id', 'code', 'created')
 # a = [{'id': '', 'code': '', 'created': ''}, {}, {}]
+
 b = sni_mod.Snippet.objects.filter(title='haha').values_list('id', 'code', 'created')
 # b = [('id', 'code', 'created'), (), ()...]
+
 c = sni_mod.Snippet.objects.filter(title='haha').values_list('code', flat=True)
 # c = ['code', ...]
+
+# in操作
+d = sni_mod.Snippet.objects.filter(title__in=['hehe', 'haha']).values_list('code', flat=True)
+
+# distinct操作，需要加order_by, 不然不起作用
+acc_mod.User.objects.filter().values_list('password', flat=True).order_by('password').distinct()
+
+# 或查询
+objs = acc_mod.User.objects.filter()
+pp = ['1', '1234']
+filter_ = Q()
+for p in pp:
+    filter_ |= Q(password=p)
+objs.filter(filter_)
+
--- utils/orm_test.py
+++ utils/orm_test.py
@@ -1,5 +1,7 @@
 import os
 
+from django.db.models import Q
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'tutorial.settings')
 
 import django
@@ -9,8 +11,22 @@ django.setup()
 from account import models as acc_mod
 from snippets import models as sni_mod
 
+# objs = acc_mod.User.objects.filter()
+# objs.filter(password=12345).update(password=1)   # 返回更新个数
+# print(objs)
+# for i in objs.all():
+#     print(i.__dict__)
+
 objs = acc_mod.User.objects.filter()
-objs.filter(password=12345).update(password=1)   # 返回更新个数
-print(objs)
-for i in objs.all():
-    print(i.__dict__)
+
+pp = ['1', '1234']
+q = Q()
+for p in pp:
+    q |= Q(password=p)
+
+# objs = objs.filter(q)
+# for i in objs:
+#     print(i)
+
+a = objs.values_list('password', flat=True).order_by('password').distinct()
+print(a)
