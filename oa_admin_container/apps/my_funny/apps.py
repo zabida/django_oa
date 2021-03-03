8765a69614d7d075d712925a3ded5e5dda749330
zabida <8888@qq.com>
2021-03-03 19:07:04 +0800

    初始化

---
+++ oa_admin_container/apps/my_funny/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyFunnyConfig(AppConfig):
+    name = 'my_funny'
---
+++ oa_admin_container/apps/public_models/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PublicModelsConfig(AppConfig):
+    name = 'public_models'
