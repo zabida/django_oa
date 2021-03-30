0109a85f7a5b47a20166a0258e89a5a45c3cd395
zabida <8888@qq.com>
2021-03-30 18:59:43 +0800

    升级

--- oa_admin_container/account/models.py
+++ oa_admin_container/account/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Permission(models.Model):
+
+    name = models.CharField(help_text='权限名字', blank=False, null=False, unique=True)
+    code = models.CharField(help_text="权限code", blank=False, null=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'permission'
+
+
+class Role(models.Model):
+    role_name = models.CharField(help_text='角色名称', blank=False, null=False, unique=True)
+    permission = models.CharField(help_text='权限', blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
